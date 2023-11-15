df10b38fd1ae784d24851c02d463e6aa76d36432
Inu Jung <inujung@momenti.tv>
2023-11-16 03:31:53 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/others.lua

--- private_dot_config/nvim/lua/plugins/others.lua
+++ private_dot_config/nvim/lua/plugins/others.lua
@@ -349,30 +349,102 @@ return {
     { "s", mode = { "n", "x", "o" }, false},
    },
 },
-{
-  'nvim-orgmode/orgmode',
-  dependencies = {
-    { 'nvim-treesitter/nvim-treesitter', lazy = true },
-  },
-  event = 'VeryLazy',
-  config = function()
-    -- Load treesitter grammar for org
-    require('orgmode').setup_ts_grammar()
+-- {
+--   'nvim-orgmode/orgmode',
+--   dependencies = {
+--     { 'nvim-treesitter/nvim-treesitter', lazy = true },
+--   },
+--   event = 'VeryLazy',
+--   config = function()
+--     -- Load treesitter grammar for org
+--     require('orgmode').setup_ts_grammar()
+--
+--     -- Setup treesitter
+--     require('nvim-treesitter.configs').setup({
+--       highlight = {
+--         enable = true,
+--         additional_vim_regex_highlighting = { 'org' },
+--       },
+--       ensure_installed = { 'org' },
+--     })
+--
+--     -- Setup orgmode
+--     require('orgmode').setup({
+--       org_agenda_files = '~/orgfiles/**/*',
+--       org_default_notes_file = '~/orgfiles/refile.org',
+--     })
+--   end,
+-- },
+ {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},
+          ["core.concealer"] = {
+          config = {
+            icon_preset = "varied",
+            icons = {
+              delimiter = {
+                horizontal_line = {
+                  highlight = "@neorg.delimiters.horizontal_line",
+                },
+              },
+              code_block = {
+                -- If true will only dim the content of the code block (without the
+                -- `@code` and `@end` lines), not the entirety of the code block itself.
+                content_only = true,
 
-    -- Setup treesitter
-    require('nvim-treesitter.configs').setup({
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'org' },
-      },
-      ensure_installed = { 'org' },
-    })
+                -- The width to use for code block backgrounds.
+                --
+                -- When set to `fullwidth` (the default), will create a background
+                -- that spans the width of the buffer.
+                --
+                -- When set to `content`, will only span as far as the longest line
+                -- within the code block.
+                  
+                width = "content",
 
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
-    })
-  end,
-}
+                -- Additional padding to apply to either the left or the right. Making
+                -- these values negative is considered undefined behaviour (it is
+                -- likely to work, but it's not officially supported).
+                padding = {
+                  -- left = 20,
+                  -- right = 20,
+                },
+
+                -- If `true` will conceal (hide) the `@code` and `@end` portion of the code
+                -- block.
+                conceal = true,
+
+                nodes = { "ranged_verbatim_tag" },
+                highlight = "CursorLine",
+                -- render = module.public.icon_renderers.render_code_block,
+                insert_enabled = true,
+              },
+            },
+          },
+        },
+          ["core.completion"] = {
+            config = {
+              engine= "nvim-cmp"
+            }
+          },
+          ["core.dirman"] = {
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+
+      vim.wo.foldlevel = 99
+      vim.wo.conceallevel = 2
+    end,
+  }
 }
