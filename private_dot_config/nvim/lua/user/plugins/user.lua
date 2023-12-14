dbed960ae44c3928304c1d4b0799b757c5a2a8cb
Inu Jung <inujung@momenti.tv>
2023-12-14 23:44:20 +0900

    Update .config/nvim/lua/user/plugins/user.lua

--- private_dot_config/nvim/lua/user/plugins/user.lua
+++ private_dot_config/nvim/lua/user/plugins/user.lua
@@ -1,155 +1,154 @@
 return {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
 
-		opts = {},
-		event = "User AstroFile",
-		cmd = { "TodoQuickFix" },
-		keys = {
-			{
-				"<leader>T",
-				"<cmd>TodoTelescope<cr>",
-				desc = "Open TODOs in Telescope",
-			},
-		},
-	},
-	{
-		"m4xshen/hardtime.nvim",
-		opts = {},
-	},
-	{
-		"simrat39/symbols-outline.nvim",
-		cmd = "SymbolsOutline",
-		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-		config = true,
-	},
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      {
+        "<leader>T",
+        "<cmd>TodoTelescope<cr>",
+        desc = "Open TODOs in Telescope",
+      },
+    },
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    opts = {},
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
 
-	-- {
-	--   "nvim-lualine/lualine.nvim",
-	--   event = "VeryLazy",
-	--   opts = function(_, opts) table.insert(opts.sections.lualine_x, "😄") end,
-	-- },
-	{
-		"kdheepak/lazygit.nvim",
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	-- {
-	--   "L3MON4D3/LuaSnip",
-	--   keys = function() return {} end,
-	-- },
-	{
-		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
-	},
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   opts = function(_, opts) table.insert(opts.sections.lualine_x, "😄") end,
+  -- },
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  -- {
+  --   "L3MON4D3/LuaSnip",
+  --   keys = function() return {} end,
+  -- },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+  },
 
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			flavour = "mocha",
-			transparent_background = true,
-			integrations = {
-				alpha = true,
-				aerial = true,
-				dap = { enabled = true, enable_ui = true },
-				mason = true,
-				neotree = true,
-				notify = true,
-				nvimtree = false,
-				semantic_tokens = true,
-				symbols_outline = true,
-				telescope = true,
-				ts_rainbow = false,
-				which_key = true,
-			},
-		},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		optional = true,
-		opts = {
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		},
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end,
-	},
-	{
-		"xbase-lab/xbase",
-		build = "make install", -- or "make install && make free_space" (not recommended, longer build time)
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"nvim-telescope/telescope.nvim", -- optional
-			"nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
-			-- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
-		},
-		config = function()
-			require("lspconfig").sourcekit.setup({})
-			require("xbase").setup({}) -- see default configuration bellow
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{
-				"Saecki/crates.nvim",
-				event = { "BufRead Cargo.toml" },
-				opts = {
-					src = {
-						cmp = { enabled = true },
-					},
-				},
-			},
-			{ "hrsh7th/cmp-emoji" },
-		},
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			local cmp = require("cmp")
-			table.insert(opts.sources, { name = "emoji" })
-			opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-				{ name = "crates" },
-			}))
-		end,
-	},
-	{ "tpope/vim-fugitive", cmd = "Git" },
-	{
-		"KadoBOT/nvim-spotify",
-		dependencies = "nvim-telescope/telescope.nvim",
-		config = function()
-			local spotify = require("nvim-spotify")
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      transparent_background = true,
+      integrations = {
+        alpha = true,
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    opts = {
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
+    "xbase-lab/xbase",
+    build = "make install", -- or "make install && make free_space" (not recommended, longer build time)
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim", -- optional
+      "nvim-lua/plenary.nvim", -- optional/requirement of telescope.nvim
+      -- "stevearc/dressing.nvim", -- optional (in case you don't use telescope but something else)
+    },
+    config = function()
+      require("lspconfig").sourcekit.setup {}
+      require("xbase").setup {} -- see default configuration bellow
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "Saecki/crates.nvim",
+        event = { "BufRead Cargo.toml" },
+        opts = {
+          src = {
+            cmp = { enabled = true },
+          },
+        },
+      },
+      { "hrsh7th/cmp-emoji" },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require "cmp"
+      table.insert(opts.sources, { name = "emoji" })
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "crates" },
+      }))
+    end,
+  },
+  {
+    "KadoBOT/nvim-spotify",
+    dependencies = "nvim-telescope/telescope.nvim",
+    config = function()
+      local spotify = require "nvim-spotify"
 
-			spotify.setup({
-				-- default opts
-				status = {
-					update_interval = 10000, -- the interval (ms) to check for what's currently playing
-					format = "%s %t by %a", -- spotify-tui --format argument
-				},
-			})
-		end,
-		build = "make",
-	},
+      spotify.setup {
+        -- default opts
+        status = {
+          update_interval = 10000, -- the interval (ms) to check for what's currently playing
+          format = "%s %t by %a", -- spotify-tui --format argument
+        },
+      }
+    end,
+    build = "make",
+  },
 
-	{
-		"Saecki/crates.nvim",
-		event = { "BufRead Cargo.toml" },
-		opts = {
-			src = {
-				cmp = { enabled = true },
-			},
-		},
-	},
+  {
+    "Saecki/crates.nvim",
+    event = { "BufRead Cargo.toml" },
+    opts = {
+      src = {
+        cmp = { enabled = true },
+      },
+    },
+  },
 } --
