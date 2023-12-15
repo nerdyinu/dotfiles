7589f53f1e0bd61294c17133b3a822727f162b39
inujung <inujung@momenti.tv>
2023-12-15 11:26:13 +0900

    Update .config/nvim.bak/LICENSE Update .config/nvim.bak/.git/config Update .config/nvim.bak/.git/index Update .config/nvim.bak/.git/logs/HEAD Update .config/nvim.bak/.git/logs/refs/heads/main Update .config/nvim.bak/.git/logs/refs/remotes/origin/main Update .config/nvim.bak/.git/refs/heads/main Update .config/nvim.bak/.git/refs/remotes/origin/main Update .config/nvim.bak/.gitignore Update .config/nvim.bak/.neoconf.json Update .config/nvim.bak/init.lua Update .config/nvim.bak/lua/plugins/neo-tree.lua Update .config/nvim.bak/lua/plugins/telescope.lua Update .config/nvim/LICENSE Update .config/nvim/.git/config Update .config/nvim/.git/index Update .config/nvim/.git/logs/HEAD Update .config/nvim/.git/logs/refs/heads/main Update .config/nvim/.git/logs/refs/remotes/origin/main Update .config/nvim/.git/refs/heads/main Update .config/nvim/.git/refs/remotes/origin/main Update .config/nvim/.gitignore Update .config/nvim/.neoconf.json Update .config/nvim/init.lua Update .config/nvim/lua/plugins/neo-tree.lua Update .config/nvim/lua/plugins/telescope.lua Change attributes of .config/nvim.bak/.git/FETCH_HEAD Change attributes of .config/nvim/.git/FETCH_HEAD

--- private_dot_config/nvim.bak/lua/plugins/neo-tree.lua
+++ private_dot_config/nvim.bak/lua/plugins/neo-tree.lua
@@ -1,41 +1,54 @@
-function system_open(path)
-  if true then
-    return {}
-  end
-  if vim.ui.open then
-    return vim.ui.open(path)
-  end
-  local cmd
-  if vim.fn.has("win32") == 1 and vim.fn.executable("explorer") == 1 then
-    cmd = { "cmd.exe", "/K", "explorer" }
-  elseif vim.fn.has("unix") == 1 and vim.fn.executable("xdg-open") == 1 then
-    cmd = { "xdg-open" }
-  elseif (vim.fn.has("mac") == 1 or vim.fn.has("unix") == 1) and vim.fn.executable("open") == 1 then
-    cmd = { "open" }
-  end
-  if not cmd then
-    notify("Available system opening tool not found!", vim.log.levels.ERROR)
-  end
-  vim.fn.jobstart(vim.fn.extend(cmd, { path or vim.fn.expand("<cfile>") }), { detach = true })
-end
-
-function extend_tbl(default, opts)
-  opts = opts or {}
-  return default and vim.tbl_deep_extend("force", default, opts) or opts
-end
-
-function notify(msg, type, opts)
-  vim.schedule(function()
-    vim.notify(msg, type, extend_tbl({ title = "AstroNvim" }, opts))
-  end)
-end
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    opts = {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "main", -- HACK: force neo-tree to checkout `main` for initial v3 migration since default branch has changed
+  dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = "Neotree",
+  init = function() vim.g.neo_tree_remove_legacy_commands = true end,
+  opts = function()
+    local utils = require "astronvim.utils"
+    local get_icon = utils.get_icon
+    return {
+      auto_clean_after_session_restore = true,
+      close_if_last_window = true,
+      sources = { "filesystem", "buffers", "git_status" },
+      source_selector = {
+        winbar = true,
+        content_layout = "center",
+        sources = {
+          { source = "filesystem", display_name = get_icon("FolderClosed", 1, true) .. "File" },
+          { source = "buffers", display_name = get_icon("DefaultFile", 1, true) .. "Bufs" },
+          { source = "git_status", display_name = get_icon("Git", 1, true) .. "Git" },
+          { source = "diagnostics", display_name = get_icon("Diagnostic", 1, true) .. "Diagnostic" },
+        },
+      },
+      default_component_configs = {
+        indent = { padding = 0 },
+        icon = {
+          folder_closed = get_icon "FolderClosed",
+          folder_open = get_icon "FolderOpen",
+          folder_empty = get_icon "FolderEmpty",
+          folder_empty_open = get_icon "FolderEmpty",
+          default = get_icon "DefaultFile",
+        },
+        modified = { symbol = get_icon "FileModified" },
+        git_status = {
+          symbols = {
+            added = get_icon "GitAdd",
+            deleted = get_icon "GitDelete",
+            modified = get_icon "GitChange",
+            renamed = get_icon "GitRenamed",
+            untracked = get_icon "GitUntracked",
+            ignored = get_icon "GitIgnored",
+            unstaged = get_icon "GitUnstaged",
+            staged = get_icon "GitStaged",
+            conflict = get_icon "GitConflict",
+          },
+        },
+      },
       commands = {
         system_open = function(state)
-          system_open(state.tree:get_node():get_id())
+          -- TODO: just use vim.ui.open when dropping support for Neovim <0.10
+          (vim.ui.open or require("astronvim.utils").system_open)(state.tree:get_node():get_id())
         end,
         parent_or_close = function(state)
           local node = state.tree:get_node()
@@ -63,47 +76,70 @@ return {
           local filename = node.name
           local modify = vim.fn.fnamemodify
 
-          local results = {
-            e = { val = modify(filename, ":e"), msg = "Extension only" },
-            f = { val = filename, msg = "Filename" },
-            F = { val = modify(filename, ":r"), msg = "Filename w/o extension" },
-            h = { al = modify(filepath, ":~"), msg = "Path relative to Home" },
-            p = { val = modify(filepath, ":."), msg = "Path relative to CWD" },
-            P = { val = filepath, msg = "Absolute path" },
+          local vals = {
+            ["BASENAME"] = modify(filename, ":r"),
+            ["EXTENSION"] = modify(filename, ":e"),
+            ["FILENAME"] = filename,
+            ["PATH (CWD)"] = modify(filepath, ":."),
+            ["PATH (HOME)"] = modify(filepath, ":~"),
+            ["PATH"] = filepath,
+            ["URI"] = vim.uri_from_fname(filepath),
           }
 
-          local messages = {
-            { "\nChoose to copy to clipboard:\n", "Normal" },
-          }
-          for i, result in pairs(results) do
-            if result.val and result.val ~= "" then
-              vim.list_extend(messages, {
-                { ("%s."):format(i), "Identifier" },
-                { (" %s: "):format(result.msg) },
-                { result.val, "String" },
-                { "\n" },
-              })
+          local options = vim.tbl_filter(function(val) return vals[val] ~= "" end, vim.tbl_keys(vals))
+          if vim.tbl_isempty(options) then
+            utils.notify("No values to copy", vim.log.levels.WARN)
+            return
+          end
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = "Choose to copy to clipboard:",
+            format_item = function(item) return ("%s: %s"):format(item, vals[item]) end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              utils.notify(("Copied: `%s`"):format(result))
+              vim.fn.setreg("+", result)
             end
-          end
-          vim.api.nvim_echo(messages, false, {})
-          local result = results[vim.fn.getcharstr()]
-          if result and result.val and result.val ~= "" then
-            vim.notify("Copied: " .. result.val)
-            vim.fn.setreg("+", result.val)
-          end
+          end)
+        end,
+        find_in_dir = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          require("telescope.builtin").find_files {
+            cwd = node.type == "directory" and path or vim.fn.fnamemodify(path, ":h"),
+          }
         end,
       },
       window = {
+        width = 30,
         mappings = {
+          ["<space>"] = false, -- disable space until we figure out which-key disabling
           ["[b"] = "prev_source",
           ["]b"] = "next_source",
-          o = "open",
+          F = utils.is_available "telescope.nvim" and "find_in_dir" or nil,
           O = "system_open",
+          Y = "copy_selector",
           h = "parent_or_close",
           l = "child_or_open",
-          Y = "copy_selector",
+          o = "open",
+        },
+        fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
+          ["<C-j>"] = "move_cursor_down",
+          ["<C-k>"] = "move_cursor_up",
         },
       },
-    },
-  },
+      filesystem = {
+        follow_current_file = { enabled = true },
+        hijack_netrw_behavior = "open_current",
+        use_libuv_file_watcher = true,
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function(_) vim.opt_local.signcolumn = "auto" end,
+        },
+      },
+    }
+  end,
 }
--- private_dot_config/nvim.bak/lua/plugins/telescope.lua
+++ private_dot_config/nvim.bak/lua/plugins/telescope.lua
@@ -1,19 +1,37 @@
 return {
   "nvim-telescope/telescope.nvim",
-  keys = {
-    -- disable the keymap to grep files
-    { "<leader>/", false },
-    { "<leader><leader>", false },
-    -- change a keymap
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    -- add a keymap to browse plugin files
-    { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-    {
-      "<leader>fp",
-      function()
-        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
-      end,
-      desc = "Find Plugin File",
-    },
+  dependencies = {
+    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
   },
+  cmd = "Telescope",
+  opts = function()
+    local actions = require "telescope.actions"
+    local get_icon = require("astronvim.utils").get_icon
+    return {
+      defaults = {
+        git_worktrees = vim.g.git_worktrees,
+        prompt_prefix = get_icon("Selected", 1),
+        selection_caret = get_icon("Selected", 1),
+        path_display = { "truncate" },
+        sorting_strategy = "ascending",
+        layout_config = {
+          horizontal = { prompt_position = "top", preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = { q = actions.close },
+        },
+      },
+    }
+  end,
+  config = require "plugins.configs.telescope",
 }
