45a4bc839f9b9e7a913eb05d89eeef3832f66b2d
inujung <inujung@momenti.tv>
2024-01-25 19:54:14 +0900

    Update .config/nvim/.git/index Update .config/nvim/lua/plugins/others.lua

--- private_dot_config/nvim/lua/plugins/others.lua
+++ private_dot_config/nvim/lua/plugins/others.lua
@@ -27,7 +27,16 @@ return {
   -- add gruvbox
 
   { "max397574/better-escape.nvim", event = "InsertCharPre", opts = { timeout = 300 } },
-
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
   {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
