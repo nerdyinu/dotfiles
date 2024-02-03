0b16aea3df82cd28ce95463b791ed3ea0ffbc20a
inujung <inujung@momenti.tv>
2024-02-04 00:42:28 +0900

    Update .config/nvim/lua/plugins/others.lua

--- private_dot_config/nvim/lua/plugins/others.lua
+++ private_dot_config/nvim/lua/plugins/others.lua
@@ -141,6 +141,7 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
         astro = {},
+        elixirls = {},
         rust_analyzer = {
           settings = {
             ["rust-analyzer"] = {
