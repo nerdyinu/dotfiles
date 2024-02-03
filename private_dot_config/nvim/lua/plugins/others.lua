7eb6c18495ba919c2bea9e788278fa96bebdfaca
inujung <inujung@momenti.tv>
2024-02-04 01:07:25 +0900

    Update .config/nvim/.git/index Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/others.lua

--- private_dot_config/nvim/lua/plugins/others.lua
+++ private_dot_config/nvim/lua/plugins/others.lua
@@ -129,7 +129,36 @@ return {
       end,
     },
   },
+  -- elixir setup
+  {
+    "elixir-tools/elixir-tools.nvim",
+    version = "*",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local elixir = require("elixir")
+      local elixirls = require("elixir.elixirls")
 
+      elixir.setup({
+        nextls = { enable = true },
+        credo = {},
+        elixirls = {
+          enable = true,
+          settings = elixirls.settings({
+            dialyzerEnabled = true,
+            enableTestLenses = false,
+          }),
+          on_attach = function(client, bufnr)
+            vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
+            vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
+          end,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
   -- add pyright to lspconfig
   {
     "neovim/nvim-lspconfig",
@@ -141,7 +170,7 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
         astro = {},
-        elixirls = {},
+
         rust_analyzer = {
           settings = {
             ["rust-analyzer"] = {
