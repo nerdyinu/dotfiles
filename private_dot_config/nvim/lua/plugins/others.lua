ba25a444b9d70d79051ec5020e95b452b055a713
inujung <inujung@momenti.tv>
2024-03-01 21:46:52 +0900

    Update .config/hypr/configs/Keybinds.conf Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/others.lua

--- private_dot_config/nvim/lua/plugins/others.lua
+++ private_dot_config/nvim/lua/plugins/others.lua
@@ -227,66 +227,66 @@ return {
   },
 
   -- add tsserver and setup with typescript.nvim instead of lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- tsserver will be automatically installed with mason and loaded with lspconfig
-
-        tsserver = {
-          -- taken from https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
-          settings = {
-            javascript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
-            },
-            typescript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all",
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
-            },
-          },
-        },
-      },
-      -- you can do any additional lsp server setup here
-      -- return true if you don't want this server to be setup with lspconfig
-      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-      setup = {
-        -- example to setup with typescript.nvim
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
-        -- Specify * to use this function as a fallback for any server
-        -- ["*"] = function(server, opts) end,
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "jose-elias-alvarez/typescript.nvim",
+  --     init = function()
+  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
+  --         -- stylua: ignore
+  --         vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+  --       end)
+  --     end,
+  --   },
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- tsserver will be automatically installed with mason and loaded with lspconfig
+  --
+  --       tsserver = {
+  --         -- taken from https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
+  --         settings = {
+  --           javascript = {
+  --             inlayHints = {
+  --               includeInlayEnumMemberValueHints = true,
+  --               includeInlayFunctionLikeReturnTypeHints = true,
+  --               includeInlayFunctionParameterTypeHints = true,
+  --               includeInlayParameterNameHints = "all",
+  --               includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --               includeInlayPropertyDeclarationTypeHints = true,
+  --               includeInlayVariableTypeHints = true,
+  --             },
+  --           },
+  --           typescript = {
+  --             inlayHints = {
+  --               includeInlayEnumMemberValueHints = true,
+  --               includeInlayFunctionLikeReturnTypeHints = true,
+  --               includeInlayFunctionParameterTypeHints = true,
+  --               includeInlayParameterNameHints = "all",
+  --               includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --               includeInlayPropertyDeclarationTypeHints = true,
+  --               includeInlayVariableTypeHints = true,
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --     -- you can do any additional lsp server setup here
+  --     -- return true if you don't want this server to be setup with lspconfig
+  --     ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+  --     setup = {
+  --       -- example to setup with typescript.nvim
+  --       tsserver = function(_, opts)
+  --         require("typescript").setup({ server = opts })
+  --         return true
+  --       end,
+  --       -- Specify * to use this function as a fallback for any server
+  --       -- ["*"] = function(server, opts) end,
+  --     },
+  --   },
+  -- },
 
   -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
   -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
