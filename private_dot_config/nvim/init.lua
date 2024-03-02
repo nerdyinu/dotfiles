ab7fb1f2af384583801cb29fbd4408e93a8f822f
inujung <inujung@momenti.tv>
2024-03-02 15:27:15 +0900

    Update .config/nvim/.git/index Update .config/nvim/init.lua Update .config/nvim/lazy-lock.json

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -11,29 +11,3 @@ parser_config.nu = {
   },
   filetype = "nu",
 }
-require("typescript-tools").setup({
-  on_attach = function()
-    require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set("n", "<leader>co", "TSToolsOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-      vim.keymap.set("n", "<leader>cR", "TSToolsRenameFile", { desc = "Rename File", buffer = buffer })
-    end)
-  end,
-  settings = {
-    tsserver_file_preferences = {
-      includeInlayEnumMemberValueHints = true,
-      includeInlayFunctionLikeReturnTypeHints = true,
-      includeInlayFunctionParameterTypeHints = true,
-      includeInlayParameterNameHints = "all",
-      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-      includeInlayPropertyDeclarationTypeHints = true,
-      includeInlayVariableTypeHints = true,
-      includeCompletionsForModuleExports = true,
-      quotePreference = "auto",
-    },
-    tsserver_format_options = {
-      allowIncompleteCompletions = true,
-      allowRenameOfImportPath = true,
-    },
-  },
-})
