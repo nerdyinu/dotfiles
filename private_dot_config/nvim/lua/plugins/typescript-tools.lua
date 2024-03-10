46093a2682ddc5f5117ad7deb1aa8c7a0c2dce38
inujung <inujung@momenti.tv>
2024-03-11 01:55:01 +0900

    Add .config/nvim/lua/plugins/typescript-tools.lua

---
+++ private_dot_config/nvim/lua/plugins/typescript-tools.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+    config = function()
+      require("typescript-tools").setup({
+
+        settings = {
+          -- spawn additional tsserver instance to calculate diagnostics on it
+          separate_diagnostic_server = true,
+          -- "change"|"insert_leave" determine when the client asks the server about diagnostic
+          publish_diagnostic_on = "insert_leave",
+          tsserver_format_options = {
+            allowIncompleteCompletions = true,
+            allowRenameOfImportPath = true,
+          },
+          tsserver_file_preferences = {
+            includeInlayEnumMemberValueHints = true,
+            includeInlayFunctionLikeReturnTypeHints = true,
+            includeInlayFunctionParameterTypeHints = true,
+            includeInlayParameterNameHints = "all",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+            includeInlayPropertyDeclarationTypeHints = true,
+            includeInlayVariableTypeHints = true,
+            includeCompletionsForModuleExports = true,
+            quotePreference = "auto",
+          },
+        },
+      })
+    end,
+  },
+}
