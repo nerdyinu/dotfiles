ef902792cab654864db81bee507ee4c586ec0add
inujung <inujung@momenti.tv>
2024-04-07 18:53:24 +0900

    Add .config/nvim/lua/plugins/conform.lua Add .config/nvim/lua/plugins/import-cost.lua

---
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      require("conform").setup({
+        formatters_by_ft = {
+          javascript = { "biome" },
+          javascriptreact = { "biome" },
+          typescript = { "biome" },
+          typescriptreact = { "biome" },
+          ["vue"] = { "biome" },
+          ["css"] = { "biome" },
+          ["scss"] = { "biome" },
+          ["less"] = { "biome" },
+          ["html"] = { "biome" },
+          ["json"] = { "biome" },
+          ["jsonc"] = { "biome" },
+          ["yaml"] = { "biome" },
+          ["markdown"] = { "biome" },
+          ["markdown.mdx"] = { "biome" },
+          ["graphql"] = { "biome" },
+          ["handlebars"] = { "biome" },
+        },
+        format_on_save = {
+          -- I recommend these options. See :help conform.format for details.
+          lsp_fallback = true,
+          timeout_ms = 500,
+        },
+      })
+    end,
+  },
+}
