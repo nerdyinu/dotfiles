996f65901ba7048eb1f5bc66569a22f93f17785f
inujung <inujung@momenti.tv>
2024-12-21 20:56:22 +0900

    Update .config/nvim/lua/plugins/blink.lua

--- private_dot_config/nvim/lua/plugins/blink.lua
+++ private_dot_config/nvim/lua/plugins/blink.lua
@@ -2,10 +2,16 @@ return {
   "saghen/blink.cmp",
   opts = {
     keymap = {
-      ["<C-n>"] = {},
-      ["<C-p>"] = {},
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
       ["<C-j>"] = { "select_next", "fallback" },
       ["<C-k>"] = { "select_prev", "fallback" },
+    },
+    completion = {
+      list = {
+        selection = "auto_insert",
+      },
     },
   },
 }
