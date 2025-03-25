5bf6d3221488c06cfdb807fe2624affe441238b6
inujung <inust33@cluml.com>
2025-03-25 10:24:48 +0900

    Update .config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,5 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>oi",
+  "<cmd>lua require('oil').open_float('.')<CR>",
+  { noremap = true, silent = true }
+)
 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
 local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
 ---@class ParserConfig
