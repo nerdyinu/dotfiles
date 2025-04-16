122bd9c0288982fc78ef8dd99a620bbf84099be1
inujung <inust33@cluml.com>
2025-04-16 17:50:23 +0900

    Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/plugins/conform.lua

--- private_dot_config/nvim/lua/config/autocmds.lua
+++ private_dot_config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,121 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+-- === Autocommand Groups ===
+local format_group = augroup("Format", { clear = true })
+local whitespace_group = augroup("TrimWhitespace", { clear = true })
+local ruler_group = augroup("FiletypeRulers", { clear = true })
+local tabsize_group = augroup("FiletypeTabSize", { clear = true })
+
+-- === JS/TS Specific Formatting (with typescript-tools and conform.nvim) ===
+autocmd("BufWritePre", {
+  group = format_group,
+  pattern = { "*.ts", "*.tsx", "*.jsx", "*.js" },
+  callback = function(args)
+    local tstools = require("typescript-tools.api")
+    tstools.add_missing_imports(true)
+    tstools.organize_imports(true)
+    tstools.fix_all(true)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
+
+-- === Global Format on Save (fallback for all files with LSP/conform) ===
+autocmd("BufWritePre", {
+  group = format_group,
+  callback = function(args)
+    local ft = vim.bo.filetype
+    if not vim.tbl_contains({ "typescript", "typescriptreact", "javascript", "javascriptreact" }, ft) then
+      require("conform").format({ bufnr = args.buf })
+    end
+  end,
+})
+
+-- === Trim trailing whitespace on save ===
+autocmd("BufWritePre", {
+  group = whitespace_group,
+  pattern = "*",
+  callback = function()
+    local save_cursor = vim.fn.getpos(".")
+    vim.cmd([[%s/\s\+$//e]])
+    vim.fn.setpos(".", save_cursor)
+  end,
+})
+
+-- === Ensure file ends with newline on save ===
+autocmd("BufWritePre", {
+  group = whitespace_group,
+  callback = function()
+    local last_line = vim.api.nvim_buf_get_lines(0, -2, -1, false)[1]
+    if last_line ~= "" then
+      vim.api.nvim_buf_set_lines(0, -1, -1, false, { "" })
+    end
+  end,
+})
+
+-- === Set rulers (colorcolumn) by filetype ===
+autocmd("FileType", {
+  group = ruler_group,
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.colorcolumn = "80"
+  end,
+})
+
+autocmd("FileType", {
+  group = ruler_group,
+  pattern = "rust",
+  callback = function()
+    vim.opt_local.colorcolumn = "100"
+  end,
+})
+
+autocmd("FileType", {
+  group = ruler_group,
+  pattern = "gitcommit",
+  callback = function()
+    vim.opt_local.colorcolumn = "50,72"
+  end,
+})
+
+-- === Set tab size per filetype ===
+autocmd("FileType", {
+  group = tabsize_group,
+  pattern = { "css" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+autocmd("FileType", {
+  group = tabsize_group,
+  pattern = { "graphql", "jsonc" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+
+autocmd("FileType", {
+  group = tabsize_group,
+  pattern = { "javascript", "json" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+-- fallback if needed for markdown
+autocmd("FileType", {
+  group = tabsize_group,
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
--- private_dot_config/nvim/lua/plugins/conform.lua
+++ private_dot_config/nvim/lua/plugins/conform.lua
@@ -2,94 +2,69 @@ return {
   {
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    opts = function(_, _)
+    opts = function(_, opts)
       local conform = require("conform")
       local util = require("conform.util")
-      -- Clone base prettier config
+
+      -- Deepcopy base formatter to customize multiple times
       local base = vim.deepcopy(require("conform.formatters.prettier"))
-      -- JSON (tab width 4)
-      local json_formatter = vim.deepcopy(base)
-      util.add_formatter_args(json_formatter, { "--tab-width", "4" }, { append = false })
-      ---@cast json_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_json = json_formatter
-      -- JSONC (tab width 2)
-      local jsonc_formatter = vim.deepcopy(base)
-      util.add_formatter_args(jsonc_formatter, { "--tab-width", "2" }, { append = false })
-      ---@cast jsonc_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_jsonc = jsonc_formatter
-      -- GraphQL (tab width 2)
-      local graphql_formatter = vim.deepcopy(base)
-      util.add_formatter_args(graphql_formatter, { "--tab-width", "2" }, { append = false })
-      ---@cast graphql_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_graphql = graphql_formatter
-      -- JavaScript (tab width 4)
-      local js_formatter = vim.deepcopy(base)
-      util.add_formatter_args(js_formatter, { "--tab-width", "4" }, { append = false })
-      ---@cast js_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_js = js_formatter
-      -- CSS (tab width 4)
-      local css_formatter = vim.deepcopy(base)
-      util.add_formatter_args(
-        css_formatter,
-        { "--single-quote", "true", "--tab-width", "4", "--prose-wrap", "preserve" },
-        { append = false }
-      )
-      ---@cast css_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_css = css_formatter
-      -- Markdown (prose-wrap, print-width)
-      local md_formatter = vim.deepcopy(base)
-      util.add_formatter_args(md_formatter, {
-        "--prose-wrap",
-        "always",
-        "--print-width",
-        "80",
-      }, { append = false })
-      ---@cast md_formatter conform.FormatterConfigOverride
-      conform.formatters.prettier_markdown = md_formatter
-      conform.setup({
-        log_level = vim.log.levels.DEBUG,
-        format_on_save = {
-          lsp_fallback = true,
-          timeout_ms = 500,
+
+      local function with_args(args)
+        local copy = vim.deepcopy(base)
+        util.add_formatter_args(copy, args, { append = false })
+        return copy
+      end
+
+      -- Merge new formatters
+      opts.formatters = vim.tbl_deep_extend("force", opts.formatters or {}, {
+        prettier_css = with_args({ "--single-quote", "true", "--tab-width", "4", "--prose-wrap", "preserve" }),
+        prettier_json = with_args({ "--tab-width", "4" }),
+        prettier_jsonc = with_args({ "--tab-width", "2" }),
+        prettier_graphql = with_args({ "--tab-width", "2" }),
+        prettier_js = with_args({ "--tab-width", "4" }),
+        prettier_markdown = with_args({ "--prose-wrap", "always", "--print-width", "80" }),
+        prettier = {
+          prepend_args = { "--single-quote", "true", "--tab-width", "4", "--prose-wrap", "preserve" },
         },
-        formatters = {
-          prettier_css = css_formatter,
-          prettier_json = json_formatter,
-          prettier_jsonc = jsonc_formatter,
-          prettier_graphql = graphql_formatter,
-          prettier_js = js_formatter,
-          prettier_markdown = md_formatter,
-          prettier = {
-            prepend_args = { "--single-quote", "true", "--tab-width", "4" },
-          },
-          rustfmt = {
-            prepend_args = { "--config", "group_imports=StdExternalCrate" },
-          },
-        },
-        formatters_by_ft = {
-          javascript = { "prettier_js" },
-          javascriptreact = { "eslint_d" },
-          typescript = { "eslint_d" },
-          typescriptreact = { "eslint_d" },
-          vue = { "eslint_d" },
-          css = { "prettier_css" }, -- This should be working now
-          scss = { "prettier_css" }, -- This should be working now
-          less = { "eslint_d" },
-          html = { "eslint_d" },
-          json = { "prettier_json" }, -- Removed prettierd as it might be conflicting
-          jsonc = { "prettier_jsonc" }, -- Removed prettierd as it might be conflicting
-          graphql = { "prettier_graphql" },
-          markdown = { "prettier_markdown" },
-          yaml = { "prettier" },
-          handlebars = { "eslint_d" },
-          astro = { "eslint_d" },
-          rust = { "rustfmt" },
-          toml = { "taplo" },
-          gitcommit = { "prettier_markdown" },
+        rustfmt = {
+          prepend_args = { "--config", "group_imports=StdExternalCrate" },
         },
       })
 
-      -- Force the formatter for specific filetypes
+      -- Merge formatters_by_ft
+      opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
+        javascript = { "prettier_js" },
+        javascriptreact = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+        vue = { "eslint_d" },
+        css = { "prettier_css" },
+        scss = { "prettier_css" },
+        less = { "eslint_d" },
+        html = { "eslint_d" },
+        json = { "prettier_json" },
+        jsonc = { "prettier_jsonc" },
+        graphql = { "prettier_graphql" },
+        markdown = { "prettier_markdown" },
+        yaml = { "prettier" },
+        handlebars = { "eslint_d" },
+        astro = { "eslint_d" },
+        rust = { "rustfmt" },
+        toml = { "taplo" },
+        gitcommit = { "prettier_markdown" },
+      })
+
+      -- Optional: set log level and format_on_save if needed
+      opts.log_level = vim.log.levels.DEBUG
+      opts.format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 500,
+      }
+    end,
+
+    config = function(_, opts)
+      require("conform").setup(opts)
+
       local augroup = vim.api.nvim_create_augroup("ConformCustom", { clear = true })
       vim.api.nvim_create_autocmd("FileType", {
         pattern = { "css", "scss" },
